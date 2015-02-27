0ffcf5dc5153548e318f94b0ef61e51f04b6bdba
Tyler Young <tyler@x-plane.com>
2015-02-26 19:56:42 -0600

    Added assets for Android pre-release (required before you can test in-app billing!)

--- release_android.sh
+++ release_android.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
 cordova build --release android
 jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 -keystore android-release-key.keystore platforms/android/ant-build/RealEstateInvestmentCalculator-release-unsigned.apk RealEstateInvestmentCalculator
 zipalign -v 4 platforms/android/ant-build/RealEstateInvestmentCalculator-release-unsigned.apk platforms/android/ant-build/RealEstateInvestmentCalculator.apk
